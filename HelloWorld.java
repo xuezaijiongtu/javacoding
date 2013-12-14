b3580989409622e1b33db3990fe816b4c315b2e5
xuezaijiongtu <33673540@qq.com>
2013-12-14 17:53:09 +0800

    The java program prinln 'hello world, java' to the screen

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,8 @@
+/*
+ *@Description:print 'hello world' to the screen
+ *@Author:xuezaijiongtu
+ *@Time:2013.12.14 17:52:36
+ */
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("hello world, java");
